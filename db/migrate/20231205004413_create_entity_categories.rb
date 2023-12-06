cf08b59e7bdd37c22774f6050b7d2d519c4f0528
unknown <albertkantwi@gmail.com>
2023-12-06 01:12:33 +0000

    Update migrate and schema

--- db/migrate/20231205004413_create_entity_categories.rb
+++ db/migrate/20231205004413_create_entity_categories.rb
@@ -1,6 +1,10 @@
 class CreateEntityCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :entity_categories do |t|
+      t.string :name
+      t.decimal :amount
+      t.references :user, null: false, foreign_key: true
+      t.references :group_category, null: false, foreign_key: true
 
       t.timestamps
     end
